67819417d62c9554b7134601f38bfa77c555c698
mrAbdhas <143132916+mrAbdhas@users.noreply.github.com>
2024-08-05 23:37:34 +0200

    Add UI element for displaying saved counts

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const countEl = document.getElementById("count-el");
 const incrementBtn = document.getElementById("increment-btn");
 const saveBtn = document.getElementById("save-btn");
 
+// Create a new paragraph element to UI to display saved count. 
+const saveEl = document.createElement("p");
+
+// Add text to the new paragraph.
+saveEl.textContent = "Previous entries: ";
+
+// Add attributes to the element.
+saveEl.setAttribute("id", "save-el");
+
+// Append it to the body.
+document.body.appendChild(saveEl);
+
